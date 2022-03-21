0d061cb6645731a4dfe7bea6f5a84fef5c09d2a9
mamchain <mamchain@protonmail.com>
2022-03-21 14:50:09 +0800

    add home

--- index.js
+++ index.js
@@ -1,44 +1,122 @@
 const express = require('express');
-const bodyParser = require('body-parser')
-//const mysql = require('mysql');
-//const request = require('request')
-//const lib = require('./lib.js')
-//const moment = require('moment');
-
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const request = require('request');
 const app = express();
+
+const url = 'http://127.0.0.1:8604';
+
+const conn = mysql.createConnection({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'hah',
+  password: '1234qwer',
+  database: 'hah'
+});
+
+conn.connect();
+
 app.use(express.static(__dirname + '/static', {index: 'help.html'}));
 app.use(bodyParser.json());
 
-app.all("*",function(req,res,next){
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin","*");
-  //允许的header类型
-  res.header("Access-Control-Allow-Headers","content-type");
-  //跨域允许的请求方式 
-  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-  if (req.method.toLowerCase() == 'options')
-      res.send(200);  //让options尝试请求快速结束
-  else
-      next();
+const frok = '00000000a137256624bda82aec19645b1dd8d41311ceac9b5c3e49d2822cd49f';
+
+function query(sql,params) {
+  return new Promise(fun => {
+    conn.query(sql,params,function(err,result) {
+      if (err) {
+        return;
+      }
+      fun(result);
+    });
+  });
+};
+
+function hah_method(method,params) {
+  return new Promise(fun => {
+    request({
+        url: url,
+        method: 'POST',
+        json: true,
+        body:{'id':1,'method' : method,'jsonrpc':'2.0','params' : params}},
+      function(error, response, body) {
+        if (body.error) {
+          return;
+        } else {
+          fun(body.result);
+        }
+      });
+  });  
+}
+
+// http://127.0.0.1:7711/quotations
+app.get('/quotations', async function(req, res, next) {
+  console.log('quotations');
+  let sql = 'SELECT tradePairId,price,`precision`,price24h FROM quotations';
+  let ret = await query(sql,[req.query.walletId]);
+  let dataString = JSON.stringify(ret);
+  res.send(JSON.parse(dataString));
 });
 
-// 最后版本信息
-app.get('/lastVersion', function(req, res, next) {
-  res.send([{"date":"2022-01-01","version":"V1.1","url":"app-debug.apk","type":"android"}]);
+app.post('/register', function(req, res, next) {
+  let bbc_addr = '';
+  let eth_addr = '';
+  let btc_addr = '';
+  for (let n = 0; n < req.body.params.wallet.length; n++) {
+    switch (req.body.params.wallet[n].chain) {
+      case 'BTC':
+        btc_addr = req.body.params.wallet[n].address;
+        break;
+      case 'ETH':
+        eth_addr = req.body.params.wallet[n].address;
+        break;
+      case 'BBC':
+        bbc_addr = req.body.params.wallet[n].address;
+        break;
+    }
+  }
+  let walletId = req.body.params.hash;
+  let sql = 'select * from addr where walletId = ?';
+  btca_conn.query(sql,[walletId],function(err,result){
+    if (err) {
+      console.log('register','err');
+      res.json({'error':err});
+      return;
+    }
+    if (result.length == 0) {
+      let pub = utils.Addr2Hex(bbc_addr);
+      pub = pub.subarray(1);
+      pub.reverse();
+      request({
+        url: url,
+        method: 'POST',
+        json: true,
+        body:{'id':2,'method':'importpubkey','jsonrpc':'2.0','params':{'pubkey': pub.toString('hex')}}
+      },function(error, response, body) {
+        sql = 'insert into addr(walletId,bbc_addr,eth_addr,btc_addr)values(?,?,?,?)';
+        btca_conn.query(sql,[walletId,bbc_addr,eth_addr,btc_addr],function(err,result) {
+          console.log('register','Add');
+          res.json({'status':'add'});
+        });
+      });
+    } else {
+      console.log('register','OK');
+      res.json({'status':'OK'});
+    }
+  });
 });
 
-// 报价信息
-app.get('/quotations', function(req, res, next) {
-  res.send([{"id":"btc/usdt","price":"2.1","change":-0.5}]);
+
+app.get('/banners', async function(req, res, next) {
+  console.log('banners');
+  let sql = 'SELECT * FROM banners';
+  let ret = await query(sql,[req.query.walletId]);
+  let dataString = JSON.stringify(ret);
+  res.send(JSON.parse(dataString));
 });
 
-// 轮播图
-app.get('/banners', function(req, res, next) {
-  res.send([{"id":"1","image":"banners1.jpg","title":"osb上线了"}]);
-});
-
-let server = app.listen(1234, function() {
+let server = app.listen(7711, function() {
   let host = server.address().address;
   let port = server.address().port;
   console.log('http://%s:%s', host, port);
-})
+});
